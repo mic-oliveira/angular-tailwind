b0b0848f737546a0a1e68fda5e4672a5bfcc4a63
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2023-05-19 02:34:42 -0300

    init product module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ResponsiveHelperComponent } from './components/responsive-helper/responsive-helper.component';
 import { ClickOutsideDirective } from './directives/click-outside.directive';
 import { DataTableComponent } from './components/data-table/data-table.component';
-import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
-  declarations: [ResponsiveHelperComponent, ClickOutsideDirective, DataTableComponent, PaginatorComponent],
+  declarations: [ResponsiveHelperComponent, ClickOutsideDirective, DataTableComponent],
   imports: [CommonModule],
-  exports: [ResponsiveHelperComponent, ClickOutsideDirective, DataTableComponent, PaginatorComponent],
+  exports: [ResponsiveHelperComponent, ClickOutsideDirective, DataTableComponent],
 })
 export class SharedModule {}
